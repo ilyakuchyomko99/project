4e619353658c224e7c96d07837cfd3f84ddee348
Ilya <ilya.kuchemko.1999@gmail.com>
2020-06-22 20:26:50 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     ismarried: false
+
+// };
+
+// let arr = ['plum.png', 'orange.jpg','apple.bmp'];
+
+
+// console.log(typeof(arr));
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(incr++);
+// console.log(--decr);
+
+// console.log(5%2);
+// console.log("2" == 2);
+
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || isClose);
